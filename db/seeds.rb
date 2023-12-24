c309be384d512d91892ebff2c77246312bc1343c
Eldar Eminov <em88el@gmail.com>
2023-12-24 21:50:29 +0300

    Update games show

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,5 @@ end
 AdminUser.create!(email: 'admin@example.com',
                   password: 'password',
                   password_confirmation: 'password') if Rails.env.development?
+
+puts 'Tables is populated!'
