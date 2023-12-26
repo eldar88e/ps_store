4606ad4efdd883238d62a221e5f80f6f1f4ab1b9
Eldar Eminov <em88el@gmail.com>
2023-12-26 21:11:00 +0300

    Update game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
-    %w[content name ps_id price old_price bonus_price img platform md5_hash deleted run_id touched_run_id genre rus_voice rus_screen]
+    %w[content name ps_id price old_price bonus_price img platform
+       md5_hash deleted run_id touched_run_id genre rus_voice rus_screen nps_id]
   end
 
   def self.ransackable_associations(auth_object = nil)
