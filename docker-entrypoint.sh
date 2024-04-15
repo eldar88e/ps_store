65e25a9681f7e08a2e57cf877013050613fab339
Eldar Eminov <em88el@gmail.com>
2024-04-15 13:20:25 +0300

    Update docker-compose.yml

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,5 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-RAILS_ENV=production bundle exec ./bin/rails assets:precompile
-#RAILS_ENV=production bundle exec ./bin/rails db:reset
+bundle exec ./bin/rails assets:precompile
 bundle exec ./bin/rails s -b 0.0.0.0
