83e3b5d145fdb512aa53c26eeb30fba66ef3d076
Eldar Eminov <em88el@gmail.com>
2024-05-14 16:53:54 +0300

    Add masterkey to .env

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,7 @@
 class Client < ApplicationRecord
   has_many :orders
   validates :email, uniqueness: true
-  validates :phone, presence: true, presence: true
+  validates :phone, presence: true
 
   validates :phone, format: { with: /\A[0-9\+\-\(\)]{10,}\z/, message: "должен содержать только цифры, плюсы, минусы, скобки и быть длиной не менее 10 символов" }
 end
