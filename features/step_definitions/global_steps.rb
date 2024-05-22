b100565de4436e49ab4812840a5939b30c705e81
Eldar Eminov <em88el@gmail.com>
2024-05-22 16:32:12 +0300

    Fix Cucumber: features/support/env.rb

--- features/step_definitions/global_steps.rb
+++ features/step_definitions/global_steps.rb
@@ -3,28 +3,23 @@
 end
 
 То /^Я должен видеть кнопку с текстом "(.*?)" и id (.+)$/ do |text, btn_id|
-  page.document.synchronize(5) do
+  page.document.synchronize(1) do
     page.should have_css "##{btn_id}", text: text
   end
 end
 
 Когда /^Я кликнул по кнопке с id (.+)$/ do |btn_id|
-  page.document.synchronize(10) do
-    find("##{btn_id}").click
-  end
-end
-
-То /^Я должен видеть кнопку с текстом "(.*?)" и с классом (.+)$/ do |text, klass|
-  page.document.synchronize(5) do
-    page.should have_css ".#{klass}", text: text
-  end
+  find("##{btn_id}").click
 end
 
 Когда /^Я залогинился как пользователь$/ do
   visit '/users/sign_in'
   fill_in "user_email", with: @user.email
   fill_in "user_password", with: @user.password
-  sleep 1
-  binding.pry
   find('input[type="submit"]').click # click_button "Log In"
+end
+
+То /^Я должен видеть кнопку с текстом "(.*?)" и с классом (.+)$/ do |text, klass|
+  sleep 0.5
+  page.should have_css ".#{klass}", text: text
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,7 @@
 require 'cucumber/rails'
 require 'RSpec/rails'
 require Rails.root.join('spec', 'support', 'capybara')
-require 'database_cleaner'
+require 'database_cleaner/active_record'
 
 ActionController::Base.allow_rescue = false
 
@@ -11,17 +11,22 @@ Before do
 end
 
 begin
-  DatabaseCleaner.strategy = :transaction
+  DatabaseCleaner.strategy = :truncation
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
 
-Around do |scenario, block|
-  DatabaseCleaner.cleaning(&block)
+Before do
+  DatabaseCleaner.start
+end
+
+After do
+  DatabaseCleaner.clean
 end
 
 Cucumber::Rails::Database.javascript_strategy = :truncation
-Capybara.default_driver = :selenium_chrome
+
+Capybara.default_driver = :selenium_chrome_headless
 Capybara.javascript_driver = :selenium_chrome
 
 ENV['CR_REMOVE_RACK_TEST_HELPERS'] = 'true'
