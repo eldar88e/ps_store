ae894449877b1b44f64e590a111685bdb6acb1ac
Eldar Eminov <em88el@gmail.com>
2024-05-25 12:48:10 +0300

    Update docker-entrypoint-test.sh

--- docker-entrypoint-test.sh
+++ docker-entrypoint-test.sh
@@ -7,7 +7,7 @@ if [ -f tmp/pids/server.pid ]; then
 fi
 
 bundle exec rails assets:precompile
-#bundle exec rails db:prepare
+bundle exec rails db:prepare
 
 bundle exec rspec
 #bundle exec cucumber
