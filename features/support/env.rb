f61ef6b77a62db20b5ce3aeb90ac958cf917a21c
Eldar Eminov <em88el@gmail.com>
2024-05-27 16:01:33 +0300

    Update ci

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,6 +12,7 @@ end
 
 begin
   DatabaseCleaner.strategy = :truncation
+  DatabaseCleaner.allow_remote_database_url = true
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
 end
