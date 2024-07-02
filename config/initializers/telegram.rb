622a90f88ec4feec01ba18c0172f51d682c5cd0d
Eldar Eminov <em88el@gmail.com>
2024-07-03 00:32:06 +0300

    Fix telegram initalizer for test env

--- config/initializers/telegram.rb
+++ config/initializers/telegram.rb
@@ -1,4 +1,4 @@
 Rails.application.configure do
-  TELEGRAM_BOT_TOKEN = ENV.fetch("TELEGRAM_BOT_TOKEN")
-  TELEGRAM_CHAT_ID   = ENV.fetch("TELEGRAM_CHAT_ID")
+  TELEGRAM_BOT_TOKEN = ENV.fetch("TELEGRAM_BOT_TOKEN") if !Rails.env.test?
+  TELEGRAM_CHAT_ID   = ENV.fetch("TELEGRAM_CHAT_ID") if !Rails.env.test?
 end
